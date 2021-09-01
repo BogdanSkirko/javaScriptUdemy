ee38c72b89ce10cd099c2de95ae37204f6842122
Bohdan13011991 <Bogdanskirko@gmail.com>
2021-09-01 19:27:26 +0300

    tasks

---
+++ script.js
@@ -0,0 +1,41 @@
+// Declare variables called 'country', 'continent' and 'population' and  assign their values according to your own country (population in millions)
+let continent = 'Europe';
+let country = 'Ukraine';
+let population = 42_000_000;
+console.log(continent, country, population)
+// Coding Challenge #1
+// Mark and John are trying to compare their BMI (Body Mass Index), which is
+// calculated using the formula:
+// BMI = mass / height ** 2 = mass / (height * height) (mass in kg
+// and height in meter).
+// Your tasks:
+// 1. Store Mark's and John's mass and height in variables
+// 2. Calculate both their BMIs using the formula (you can even implement both
+// versions)
+// 3. Create a Boolean variable 'markHigherBMI' containing information about
+// whether Mark has a higher BMI than John.
+// Test data:
+// § Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95
+// m tall.
+// § Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76
+// m tall.
+// GOOD LUCK
+const weigthMarks = 78;
+const heigthMarks = 1.69;
+const weigthJohn = 92;
+const heigthJohn = 1.95;
+const weigthMarks1 = 95;
+const heigthMarks1 = 1.88;
+const weigthJohn1 = 85;
+const heigthJohn1 = 1.76;
+
+const bmiMarks = weigthMarks / (heigthMarks * heigthMarks);
+const bmiJohn = weigthJohn / (heigthJohn * heigthJohn);
+
+const bmiMarks1 = weigthMarks1 / heigthMarks1 ** 2;
+const bmiJohn1 = weigthJohn1 / heigthJohn1 ** 2;
+const marksHeighterBmi = bmiMarks > bmiJohn;
+const marksHeighterBmi1 = bmiMarks1 > bmiJohn1;
+
+console.log(bmiMarks, bmiJohn, marksHeighterBmi);
+console.log(bmiMarks1, bmiJohn1, marksHeighterBmi1);
