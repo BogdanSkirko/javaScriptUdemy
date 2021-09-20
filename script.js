003e15dc037a6d251b51ebaf8ce789d6eb6c04bc
Bohdan13011991 <Bogdanskirko@gmail.com>
2021-09-20 16:23:26 +0300

    tasks

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ const poll = {
     options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
     // This generates [0, 0, 0, 0]. More in the next section!
     answers: new Array(4).fill(0),
-    registerNewAnwer () {
+    registerNewAnwer() {
         const answer = +prompt(`${this.question} \n ${this.options.join(`\n`)}
         \n (Write option number)`);
         console.log(answer)
